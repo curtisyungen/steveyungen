5230a4c1f75075978d0fd4b6bab142fe6ed5bff3
Curtis Yungen <curtisyungen@gmail.com>
2020-01-03 21:13:33 -0800

    add navbar links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar/navbar";
 import Home from "./pages/Home";
+import Music from "./pages/Music";
+import Videos from "./pages/Videos";
+import Gallery from "./pages/Gallery";
+import Events from "./pages/Events";
 import "./App.css";
 
 class App extends Component {
@@ -16,6 +15,10 @@ class App extends Component {
         <Navbar />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/music" component={Music} />
+          <Route exact path="/videos" component={Videos} />
+          <Route exact path="/gallery" component={Gallery} />
+          <Route exact path="/events" component={Events} />
         </Switch>
       </Router>
     );
