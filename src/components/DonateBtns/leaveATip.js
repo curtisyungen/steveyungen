fd275c6bda083d9d915e8aaa78dee28e3628117f
Curtis Yungen <curtisyungen@gmail.com>
2020-01-05 13:39:23 -0800

    update colors on leaveATip

--- src/components/DonateBtns/leaveATip.js
+++ src/components/DonateBtns/leaveATip.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./donateBtns.css";
+import { withRouter } from "react-router-dom";
 
 class LeaveATip extends Component {
   constructor(props) {
@@ -18,13 +19,15 @@ class LeaveATip extends Component {
     const { source } = this.props;
     const { pathname } = this.state;
     return (
-      <div
+      <a
         className={`d-${
           pathname !== "/tipJar" ? "block" : "none"
-        } donateBtn db-${source} btn btn-dark btn-sm`}
+        } donateBtn db-${source} 
+        btn btn-outline-light btn-sm`}
+        href="/tipJar"
       >
-        <a href="/tipJar">Leave a Tip</a>
-      </div>
+        Leave a Tip
+      </a>
     );
   }
 }
