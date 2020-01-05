a9475b90d3d28205813984177762b2c26fe89305
Curtis Yungen <curtisyungen@gmail.com>
2020-01-05 00:07:16 -0800

    change animation on photos

--- src/components/Photo/photo.js
+++ src/components/Photo/photo.js
@@ -5,7 +5,7 @@ class Photo extends Component {
   render() {
     const { image, title, description, link, target } = this.props;
     return (
-      <a href={link} className="photo fadeIn-2" target={target}>
+      <a href={link} className="photo fadeIn-fast" target={target}>
         <img src={image} style={this.props.style} />
         <p className="title">{title}</p>
         <p className="description">{description}</p>
