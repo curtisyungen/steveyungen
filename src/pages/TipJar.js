2659dc186241a33bef24019374d13e71adead6b3
Curtis Yungen <curtisyungen@gmail.com>
2020-01-05 12:47:52 -0800

    delete tipJar.css

--- src/pages/TipJar.js
+++ src/pages/TipJar.js
@@ -2,16 +2,21 @@ import React, { Component } from "react";
 import Photo from "../components/Photo/photo";
 import DonateBtns from "../components/DonateBtns/donateBtns";
 import tipJarImg from "../images/tip-jar.jpg";
-import "./TipJar.css";
+
+const photoStyle = {
+  maxHeight: "300px",
+  maxWidth: "100%"
+};
 
 class TipJar extends Component {
   render() {
     return (
-      <div className="tipJar">
+      <div className="tipJar text-center">
         <Photo
           image={tipJarImg}
           title="Tip Jar"
           description="Tips are very much appreciated!"
+          style={photoStyle}
         />
         <DonateBtns source="tipJar" />
       </div>
