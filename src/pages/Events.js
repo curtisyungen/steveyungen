6084dc41a21b7a911bc73b33fbad905d8532f92c
Curtis Yungen <curtisyungen@gmail.com>
2020-01-06 11:56:14 -0800

    update events page

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -3,6 +3,8 @@ import Photo from "../components/Photo/photo";
 import alohaFriday from "../images/events/Aloha-Friday-SJY-11-17-2017.jpg";
 import sosaPoster from "../images/events/sosa-poster.jpg";
 
+const eventPhotos = [alohaFriday, sosaPoster];
+
 const photoStyle = {
   maxHeight: "400px",
   maxWidth: "100%"
@@ -13,8 +15,9 @@ class Events extends Component {
     return (
       <div className="container events-container">
         <div className="row row-style justify-content-center text-center">
-          <Photo image={alohaFriday} style={photoStyle} />
-          <Photo image={sosaPoster} style={photoStyle} />
+          {eventPhotos.map(event => (
+            <Photo image={event} style={photoStyle} />
+          ))}
         </div>
       </div>
     );
