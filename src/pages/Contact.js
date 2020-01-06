886f665582b50276cc43f86145551436c8276b90
Curtis Yungen <curtisyungen@gmail.com>
2020-01-05 22:26:48 -0800

    add contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,19 +11,75 @@ const photoStyle = {
 };
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+      email: null,
+      message: null
+    };
+  }
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
     return (
       <div className="container contact-container">
-        <div className="row row-style justify-content-center text-center aboutMusic">
-          <p className="title large-title">CONTACT STEVE</p>
-          <div className="spacer"></div>
-        </div>
-
-        <div className="row row-style justify-content-center text-center">
-          <a href="mailto:syungen@yahoo.com">syungen@yahoo.com</a>
-        </div>
-
         <div className="contact">
+          <div className="justify-content-center text-center aboutMusic">
+            <p className="title large-title">CONTACT STEVE</p>
+            <div className="spacer"></div>
+          </div>
+
+          {/* <div className="justify-content-center text-center">
+          <a href="mailto:syungen@yahoo.com">syungen@yahoo.com</a>
+        </div> */}
+
+          <form
+            className="contactForm"
+            method="POST"
+            action="/submitContactForm"
+          >
+            <div className="form-group">
+              <input
+                name="name"
+                type="text"
+                className="form-control"
+                id="nameInput"
+                placeholder="Your name"
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                name="email"
+                type="email"
+                className="form-control"
+                id="emailInput"
+                placeholder="Your email"
+                onChange={this.handleInputChange}
+              />
+            </div>
+            <div className="form-group">
+              <textarea
+                className="form-control"
+                id="messageInput"
+                placeholder="Your message"
+                onChange={this.handleInputChange}
+                rows="3"
+              ></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Send
+            </button>
+          </form>
+
           <div className="steve-sosa-cutout">
             <Photo image={SteveSOSACutOut} style={photoStyle} />
           </div>
