c545a69c517b84efdf1704ffc275fe1fc083a0f4
Curtis Yungen <curtisyungen@gmail.com>
2020-01-06 19:58:50 -0800

    add SocialIcons

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,32 +1,16 @@
 import React, { Component } from "react";
 import ScrollTop from "../ScrollTop/scrollTop";
+import SocialIcons from "../SocialIcons/socialIcons";
 import "./footer.css";
 
 class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pathname: null
-    };
-  }
-
-  componentDidMount = () => {
-    this.setState({
-      pathname: window.location.pathname
-    });
-  };
-
   render() {
-    const { pathname } = this.state;
     return (
-      <div
-        className={`page-footer text-center footer ${
-          pathname !== "/events" ? "mt-3" : "mt-0"
-        }`}
-      >
+      <div className={`page-footer text-center footer`}>
         <div className="footer-inner">
           <h5>Footer</h5>
         </div>
+        <SocialIcons />
         <ScrollTop />
       </div>
     );
